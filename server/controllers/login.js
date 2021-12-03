5467359a3408bd030ac5c9d576ec3aeee800fc39
Sara Vieira <saraiovieira99@gmail.com>
2021-12-03 17:39:47 +0000

    change folder's name

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,4 +1,4 @@
-import { User } from '../model/user.js';
+import { User } from '../models/user.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
@@ -17,15 +17,15 @@ export const login = async (req, res) => {
 
     if (user && (await bcrypt.compare(password, user.password))) {
        // Create token
-       const token = jwt.sign(
+      const token = jwt.sign(
         { user_id: user._id, email },
         process.env.TOKEN_KEY,
         {
           expiresIn: "2h",
         }
       );
-       // save user token
-       user.token = token;
+      // save user token
+      user.token = token;
        // user
       res.status(200).json(token);
     }
