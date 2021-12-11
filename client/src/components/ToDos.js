8bfa39fb4037943482cab02a2200beee322866a4
Sara Vieira <saraiovieira99@gmail.com>
2021-12-11 20:42:05 +0000

    add todos component

---
+++ client/src/components/ToDos.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ToDos = ({ todos, updateTodo, deleteTodo }) => {
+
+    return (
+        <> 
+            <ul>
+                {todos.map(({ _id, task, completed }, i) => (
+                    <li
+                    key={i}
+                    onClick={e => updateTodo(e, _id)}
+                    className={completed ? "completed" : ""}
+                    >
+                    {task} 
+                    <input
+                        id="todo-0" 
+                        type="checkbox" 
+                        defaultChecked={false}
+                        onClick={e => deleteTodo(e, _id)}
+                    />
+                    </li>
+                ))}
+        </ul>
+        </>
+    )
+}
+
+export default ToDos
