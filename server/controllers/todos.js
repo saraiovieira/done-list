c3a3570d683656802131cce18db1453111863a9a
Sara Vieira <saraiovieira99@gmail.com>
2021-12-23 16:22:00 +0000

    add console.log to debug errors

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -4,40 +4,40 @@ function success(res, payload) {
     return res.status(200).json(payload)
 }
 
-export const createTodo = async (req,res, next) => {
-    try {
-        const todo = await TodoTask.create(req.body)
-        return success(res, todo)
-    } catch (err) {
-        next({ status: 400, message: "failed to create todos" })
-    }
+export const createTodo = async (req,res) => {
+  try {
+    const todo = await TodoTask.create(req.body)
+    return success(res, todo)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export const getTodos = async (req,res, next) => {
-    try {
-        const todos = await TodoTask.find({})
-        return success(res, todos)
-      } catch (err) {
-        next({ status: 400, message: "failed to get todos" })
-      }
+export const getTodos = async (req,res) => {
+  try {
+    const todos = await TodoTask.find({})
+    return success(res, todos)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export const updateTodo = async (req, res, next) => {
-    try {
-      const todo = await TodoTask.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    })
-      return success(res, todo)
-    } catch (err) {
-      next({ status: 400, message: "failed to update todo" })
-    }
+export const updateTodo = async (req, res) => {
+  try {
+    const todo = await TodoTask.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+    return success(res, todo)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export const deleteTodo = async (req, res, next) => {
-    try {
-      await TodoTask.findByIdAndRemove(req.params.id)
-      return success(res, "todo deleted!")
-    } catch (err) {
-      next({ status: 400, message: "failed to delete todo" })
-    }
+export const deleteTodo = async (req, res) => {
+  try {
+    await TodoTask.findByIdAndRemove(req.params.id)
+    return success(res, "todo deleted!")
+  } catch (err) {
+    console.log(err)
+  }
 }
