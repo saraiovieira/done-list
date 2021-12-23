cce48383d513b1a30f99542190f3ec8c0908748c
Sara Vieira <saraiovieira99@gmail.com>
2021-12-23 16:24:14 +0000

    add semicolons

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import routes  from './routes/web.js';
-import cors from 'cors'
+import cors from 'cors';
 import { config } from 'dotenv';
 import { connect } from './config/database.js';
 config();
@@ -10,7 +10,7 @@ const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
