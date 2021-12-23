04fe866700739029a2166e292d59cd3dc0555574
Sara Vieira <saraiovieira99@gmail.com>
2021-12-23 16:23:03 +0000

    add css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import { createTodoAPI, deleteTodoAPI, updateTodoAPI, getAllTodosAPI } from './APIHelper';
 import Button from './components/Button';
 import Header from './components/Header';
