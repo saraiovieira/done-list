502d53b499b78eae43c64d15aa117258908b2966
Sara Vieira <saraiovieira99@gmail.com>
2021-12-31 15:40:32 +0000

    connect to api to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
 
 const Login = () => {
+
+    let navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleLogin = async e => {
+        e.preventDefault();
+        if (!email || !password) {
+            setErrorMessage('You forgot to enter your email or password!!');
+        } else {
+            try {
+                await axios.post("http://localhost:5000/login/", {
+                    email: email,
+                    password: password
+                })
+                navigate('/todolist');
+            } catch (err) {
+                console.log(err);
+                setErrorMessage('Invalid Credentials');
+            }
+        }
+       
+    }
+
     return (
         <div>
             <h2>Login</h2>
-            <input type="text" name="Email" id="email" placeholder="Enter your email" aria-label='login' />
-            <input type="password" name="Password" id="password" placeholder="Enter your password" aria-label='login'/>
-            <button type='button'>Login</button>
+            <input type="text" name="Email" id="email" value={email} onChange={({ target }) => setEmail(target.value)} placeholder="Enter your email" aria-label='login' />
+            <input type="password" name="Password" id="password" value={password} onChange={({ target }) => setPassword(target.value)} placeholder="Enter your password" aria-label='login'/>
+            <button type='button' onClick={handleLogin}>Login</button>
+            {errorMessage && (
+            <p className="error"> {errorMessage} </p>)}
         </div>
     )
 }
