14f611b7615e4fb9ae57eb2910b4a6e5561574a2
Sara Vieira <saraiovieira99@gmail.com>
2021-12-31 15:40:13 +0000

    connect to api to register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+
+    let navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleRegister = async e => {
+        e.preventDefault();
+        if (!email || !password) {
+            setErrorMessage('You forgot to enter your email or password!!');
+        } else {
+            try {
+                await axios.post("http://localhost:5000/register/", {
+                    email: email,
+                    password: password
+                })
+                navigate('/login');
+            } catch(err) {
+                console.log(err);
+                setErrorMessage('User already exists');
+            }
+        }
+                
+    }
     return (
         <div>
             <h2>Register</h2>
-            <input type="text" name="Email" id="email" placeholder="Enter your email" aria-label='login' />
-            <input type="password" name="Password" id="password" placeholder="Enter your password" aria-label='login'/>
-            <button type='button'>Register</button>
+            <input type="text" name="Email" id="email" value={email} onChange={({ target }) => setEmail(target.value)} placeholder="Enter your email" aria-label='register' />
+            <input type="password" name="Password" id="password" value={password} onChange={({ target }) => setPassword(target.value)} placeholder="Enter your password" aria-label='register'/>
+            <button type='button' onClick={handleRegister}>Register</button>
+            {errorMessage && (
+            <p className="error"> {errorMessage} </p>)}
         </div>
     )
 }
