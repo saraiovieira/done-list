daef32e9e5b63ff750e6cf48f98896a1e9a90775
saraiovieira <saravieira09@hotmail.com>
2022-03-25 11:29:02 +0000

    improve validation and remove comments

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,51 +1,64 @@
 import { User } from '../models/user.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { check, validationResult } from 'express-validator'
 import { config } from 'dotenv';
 config(); 
+
+export const validate = (method) => {
+    switch (method) {
+      case 'register': {
+       return [ 
+            check('email', 'Email is invalid')
+                .isEmail(),
+            check('password', 'Password is invalid')
+            .isLength({ min: 1 })
+        ]   
+      }
+    }
+}
 
 export const register = async (req, res) => {
 
     try {
-        // Get user input
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                success: false,
+                errors: errors.array()
+            });
+        }
+
         const { email, password } = req.body;
 
-        // Validate user input
-        if (!(email && password)) {
-        res.status(400).send("All input is required");
-        }
+        const oldUser = await User.findOne({ "email": email });
 
-        // check if user already exist
-        // Validate if user exist in the database
-        const oldUser = await User.findOne({ email });
-
+        console.log(oldUser)
         if (oldUser) {
-        return res.status(409).send("User Already Exist. Please Login");
+            return res.status(409).send("User Already Exist. Please Login");
         }
 
-        //Encrypt user password
         const salt = await bcrypt.genSalt(10)
         const encryptedPassword = await bcrypt.hash(password, salt);
 
-        // Create user in the database
         const user = await User.create({
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
+            email: email.toLowerCase(), 
+            password: encryptedPassword,
         });
 
-        // Create token
         const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-    );
-         // save user token
-         user.token = token;
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+            expiresIn: "2h",
+            }
+        );
 
-        // return new user
+        user.token = token;
         res.status(201).json(user);
+
     } catch (err) {
         console.log(err);
     }
