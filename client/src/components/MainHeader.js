fc09e9f569786ebaa5e81de3c09fe3c8973781c8
saraiovieira <saravieira09@hotmail.com>
2022-03-25 12:48:10 +0000

    remove header and add two different ones

---
+++ client/src/components/MainHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainHeader = () => {
+  return (
+    <>
+        <header>
+            <h1> Done List </h1>
+        </header>            
+    </>
+  )
+}
+
+export default MainHeader
