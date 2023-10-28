98893bb3d3410c324f1ee831e2f546a870f3e5eb
Sara Vieira <saraiovieira99@gmail.com>
2023-10-28 16:36:43 +0100

    add css to button

--- client/src/Components/DoneHeader.jsx
+++ client/src/Components/DoneHeader.jsx
@@ -11,7 +11,7 @@ const DoneHeader = () => {
     <header>
       <img className="logo" src={Logo} alt="Done list logo" />
       <h1> Your Done List </h1>
-      <button type="button" onClick={logOut}>
+      <button className="done-button" type="button" onClick={logOut}>
         Log out
       </button>
     </header>
