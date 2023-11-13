243f82567aa266991a626983e547adefe6be69ba
Sara Vieira <saraiovieira99@gmail.com>
2023-11-13 12:07:07 +0000

    add class

--- client/src/Components/DoneHeader.jsx
+++ client/src/Components/DoneHeader.jsx
@@ -13,7 +13,7 @@ const DoneHeader = () => {
     <header className="done__header">
       <div className="logo_container">
         <img className="logo" src={Logo} alt="Done list logo" />
-        <h1> Your Done List </h1>
+        <h1 className="main__title"> Your Done List </h1>
       </div>
       <FontAwesomeIcon className="logout" icon={faRightFromBracket} onClick={logOut} />
       <button className="done-button" type="button" onClick={logOut}>
--- client/src/Components/MainHeader.jsx
+++ client/src/Components/MainHeader.jsx
@@ -5,8 +5,8 @@ const MainHeader = () => {
   return (
     <>
       <header className="main__header">
-        <img className="home_logo" src={logo} alt="Done List Logo" />
-        <h1> Done List </h1>
+        <img className="logo" src={logo} alt="Done List Logo" />
+        <h1 className="main__title"> Done List </h1>
       </header>
     </>
   );
