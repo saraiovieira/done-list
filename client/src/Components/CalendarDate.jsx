0949310e2deb5c879affde50c45fd1d114cb81ba
Sara Vieira <saraiovieira99@gmail.com>
2023-12-07 17:37:21 +0000

    remove useCallback

--- client/src/Components/CalendarDate.jsx
+++ client/src/Components/CalendarDate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
