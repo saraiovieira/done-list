d633cdf400e63c090be728ad2d1334ab5cdf0f5e
Sara Vieira <saraiovieira99@gmail.com>
2023-12-11 14:55:58 +0000

    add tooltip

--- client/src/Components/DoneHeader.jsx
+++ client/src/Components/DoneHeader.jsx
@@ -15,7 +15,8 @@ const DoneHeader = () => {
         <img className="done-header__logo" src={Logo} alt="Done list logo" />
         <h1 className="done-header__main-title"> Your Done List </h1>
       </div>
-      <FontAwesomeIcon className="done-header__logout-icon" icon={faRightFromBracket} onClick={logOut} />
+      <FontAwesomeIcon className="done-header__logout-icon" title={"Log out"}
+        icon={faRightFromBracket} onClick={logOut} />
       <button className="done-header__logout-button" type="button" onClick={logOut}>
         Log out
       </button>
